e3412914ac1c4d49ba7296ebc79e52417c78dc41
NAD3R <naderdeghaili@gmail.com>
2026-02-06 10:02:10 +0100

    initial commit, connected to DB

---
+++ src/main/java/naderdeghaili/u5w1d5wp/U5W1D5WpApplication.java
@@ -0,0 +1,13 @@
+package naderdeghaili.u5w1d5wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D5WpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5W1D5WpApplication.class, args);
+    }
+
+}
